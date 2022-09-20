79570947c323df4a6df10a34c5ed5f5daef7441f
João Pedro <joao.pedreira@estudante.ufjf.br>
2022-09-20 15:03:22 -0300

    modifica comando de criar migration e cria model padrão para criar novas models automaticamente

--- bootstrap/create_migration.php
+++ bootstrap/create_migration.php
@@ -1,6 +1,6 @@
 <?php
 
-if(count($argv) == 2) {
+if(count($argv) == 2 || count($argv) == 3) {
     $migration_name = $argv[1];
     $migration_path = __DIR__."\..\database\migrations\\" . $migration_name . "_table.php";
     if(!file_exists($migration_path) || !is_file($migration_path)) {
@@ -21,7 +21,23 @@ if(count($argv) == 2) {
         $fileOpen = fopen($migration_path, "w");
         file_put_contents($migration_path, $customized_migration);
 
-        //$rename
+        if(key_exists(2, $argv)) {
+            $model_name = ucfirst(substr($migration_name, 0 , -1));
+            $model_path = __DIR__."\..\app\Models\\$model_name.php";
+            if(!file_exists($model_path) || !is_file($model_path)) {
+                $file = __DIR__."\..\app\Models\default_model\DefaultModel.php";
+                $default_model = file_get_contents($file);
+                $customized_model = str_replace(
+                    "table_name_uc_first",
+                    $model_name,
+                    $default_model
+                );
+                $fileOpen = fopen($model_path, "w");
+                file_put_contents($model_path, $customized_model);
+            } else {
+                echo "A model $model_name já existe";
+            }
+        }
     } else {
         echo "A migration já $migration_name já foi criada";
     }
