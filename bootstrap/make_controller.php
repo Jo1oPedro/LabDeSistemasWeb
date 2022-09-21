cea6a863f7a833a078c4b0e5118e3312ed6999c3
João Pedro <joao.pedreira@estudante.ufjf.br>
2022-09-21 01:19:20 -0300

    corrige comando para criar novos controllers e adiciona controller de posts

--- bootstrap/make_controller.php
+++ bootstrap/make_controller.php
@@ -4,7 +4,7 @@
 
 if(count($argv) == 2) {
     $controller_name = $argv[1];
-    $controller_path = __DIR__."\..\app\Controllers\" . $controller_name;
+    $controller_path = __DIR__."\..\app\Controllers\\" . $controller_name . ".php";
     if(!file_exists($controller_path) || !is_file($controller_path)) {
         $file = __DIR__."\..\app\Controllers\default_controller\DefaultController.php";
         $default_controller = file_get_contents($file);
@@ -20,7 +20,7 @@ if(count($argv) == 2) {
         $fileOpen = fopen($controller_path, "w");
         file_put_contents($controller_path, $customized_controller);
     } else {
-        echo "O controller já $controller_name já foi criado";
+        echo "O controller $controller_name já foi criado";
     }
 
 }
