6454085dc02dfc10fd7582099981704cb671c4ed
João Pedro <joao.pedreira@estudante.ufjf.br>
2022-09-22 17:06:14 -0300

    altera o nome de algumas migrations

--- database/migrations/16637837143157_posts_table.php
+++ database/migrations/16637837143157_posts_table.php
@@ -13,6 +13,7 @@ class poststable
             $table->increments("id");
             $table->string("body");
             $table->foreignId("user_id")->constrained()->onDelete('cascade');
+            $table->string('image_path')->default(__DIR__."\..\..\public\img\defaultImage.png");
             $table->timestamps();
         });
     }
