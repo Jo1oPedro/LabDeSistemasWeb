173f596ba331e225149bc5bac6f2a0ac68b6b1c8
João Pedro <joao.pedreira@estudante.ufjf.br>
2022-10-29 00:55:56 -0300

    retira uses inuteis da homeController

--- app/Controllers/HomeController.php
+++ app/Controllers/HomeController.php
@@ -1,13 +1,6 @@
 <?php
 
 namespace App\Controllers;
-use App\Core\App;
-use App\Models\Banner;
-use App\Models\Post;
-use App\Models\User;
-use Exception;
-use Illuminate\Database\Capsule\Manager as DB;
-
 class HomeController extends Controller
 {
     public function __construct()
